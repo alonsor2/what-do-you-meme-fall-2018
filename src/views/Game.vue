0bc3f1b38b0b2127cb7d0045c25dd57511f14f17
Robert Alonso <alonsor2@hawkmail.newpaltz.edu>
2018-10-22 12:28:16 -0400

    Edited vue webpage

---
+++ src/views/Game.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="alert alert-success">
+        Yay We Have a game!
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script>
+export default {
+    
+}
+</script>
